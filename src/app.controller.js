09d6e03f0497e22babf3f8e2daa57adbbdb6b59f
alighareeb01 <ghareeb874@gmail.com>
2026-03-13 16:46:38 +0200

    try

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,22 +1,25 @@
 import express from "express";
 import morgan from "morgan";
-import { databaseConnection } from "./database/connection.js";
 import authRouter from "./modules/authentication/authentication.controller.js";
 import userRouter from "./modules/user/user.controller.js";
 import messageRouter from "./modules/message/message.controller.js";
+import { databaseConnection } from "./database/connection.js";
 
 export const bootstrap = () => {
   const app = express();
-
   app.use(express.json());
+  app.get("/", (req, res) => {
+    res.send("Saraha API is running 🚀");
+  });
+
+  databaseConnection();
+
   app.use(morgan("dev"));
   app.use("/authentication", authRouter);
   app.use("/user", userRouter);
   app.use("/message", messageRouter);
   app.use(express.urlencoded({ extended: true }));
   app.use("/uploads", express.static("uploads"));
-  databaseConnection();
 
   return app;
-
 };
