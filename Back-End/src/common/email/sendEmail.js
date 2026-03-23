9e077094e4fb28a264be0c512df57957327da2e2
alighareeb01 <ghareeb874@gmail.com>
2026-03-23 17:53:50 +0200

    fix

--- Back-End/src/common/email/sendEmail.js
+++ Back-End/src/common/email/sendEmail.js
@@ -6,13 +6,13 @@ export const sendEmail = async (email, subject, text, html) => {
     port: 587,
     secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "ghareeb874@gmail.com",
+      pass: "inza herf borm mhwn",
     },
   });
 
   let info = await transport.sendMail({
-    from: process.env.EMAIL_USER,
+    from: `"Aly" <ghareeb874@gmail.com>`,
     to: email,
     subject: subject,
     text: text,
