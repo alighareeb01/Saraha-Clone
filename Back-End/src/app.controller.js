a05b88b946ada562985b73a44a05ec0d34aa7791
alighareeb01 <ghareeb874@gmail.com>
2026-03-23 16:38:31 +0200

    a push to refresh vercel

--- Back-End/src/app.controller.js
+++ Back-End/src/app.controller.js
@@ -9,7 +9,7 @@ import cors from "cors";
 
 
 //comment  forVERCEL ss
-//comment for vercel refress
+//comment for vercel refresh 
 export const bootstrap = () => {
   const app = express();
   app.use(express.json());
