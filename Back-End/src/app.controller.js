6c46ce28eb48289d70ad21e43fd186630db9fef2
alighareeb01 <ghareeb874@gmail.com>
2026-03-23 18:15:42 +0200

    redploy

--- Back-End/src/app.controller.js
+++ Back-End/src/app.controller.js
@@ -13,6 +13,7 @@ import cors from "cors";
 //comment for vercel refresh
 //comment for vercel refresh
 //comment for vercel refresh
+//comment for vercel refresh
 export const bootstrap = () => {
   const app = express();
   app.use(express.json());
